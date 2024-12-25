236adf8f9ede2d8330dac3e86491af92fd314b4c
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-25 11:14:25 -0800

    got the text to randomize

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-window.onload = function getDrink(){
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-    fetch("http://api.alquran.cloud/v1/ayah/262/en.asad")
+const rndInt = randomIntFromInterval(1, 6236);
+console.log(rndInt);
+
+window.onload = function getAya(){
+    let url = 'http://api.alquran.cloud/v1/ayah/'
+    fetch(`${url}${rndInt}/en.asad`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data.data)
       document.querySelector('h2').innerText = data.data.surah.englishName
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions
-      document.querySelector('h4').innerText = data.drinks[0].strInstructions
-      document.querySelector('h5').innerText = data.drinks[0].strDrink
-      document.querySelector('h6').innerText = data.drinks[0].strDrink
-      document.querySelector('p').innerText = data.drinks[0].strDrink
+      document.querySelector('h3').innerText = data.data.surah.name
+      document.querySelector('h4').innerText = data.data.surah.number
+      document.querySelector('h5').innerText = data.data.surah.numberOfAyahs
+      document.querySelector('h6').innerText = data.data.surah.revelationType
+      document.querySelector('p').innerText = data.data.text
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 }
 
-//www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
+
+// quran audio:
+// window.onload = function getAudio(){
+//   let url = 'http://api.alquran.cloud/v1/quran/ar.alafasy'
+//   fetch(`${url}`)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     console.log(data.data)
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   });
+// }
+
+//http://api.alquran.cloud/v1/ayah/262/en.asad
